11edd7d19c7d369bb1aa7d9f02c8b7ad5fd61669
ivandoza <ivandoza@gmail.com>
2023-10-19 18:07:50 +0100

    Added exercise 10

--- 10.Optional-chaining/exercise.js
+++ 10.Optional-chaining/exercise.js
@@ -1,5 +1,14 @@
-const order = {};
+const order = {
+  customer: {
+    address: {
+      city:{}         // De esta manera se imprime 'true'
+    }                 // Si quitamos la propiedad city imprimirá 'false' 'City is required'
+  },
+};
+
 
 if (order && order.customer && order.customer.address && !order.customer.address.city) {
   console.log('City is required');
 }
+
+console.log(Object.hasOwn(order.customer.address, 'city'))
