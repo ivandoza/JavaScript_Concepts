83c2e3cf5257c660b8c9b91cfe48c34929a795c8
ivandoza <ivandoza@gmail.com>
2023-10-22 19:27:31 +0100

    exercise 12 and 13

---
+++ 12.Array-methods-advanced/exercise.js
@@ -0,0 +1,25 @@
+function nicknameMap(persons) {
+  let NicksArray = [];
+  persons.forEach(nicks => {
+    NicksArray.push(nicks.name +"-"+ nicks.age); 
+  });
+  return NicksArray;
+}
+
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const nicknames = nicknameMap(persons);
+console.log(persons);
+console.log(nicknames);
