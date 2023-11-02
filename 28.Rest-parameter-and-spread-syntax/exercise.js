716e57d241b3de81b04651415d3621b3dfb17014
ivandoza <ivandoza@gmail.com>
2023-11-02 00:36:28 +0000

    Correccion ejercicio 28

--- 28.Rest-parameter-and-spread-syntax/exercise.js
+++ 28.Rest-parameter-and-spread-syntax/exercise.js
@@ -2,7 +2,11 @@ function sum(...nums) {
   let suma = 0;
   for (let num of nums) suma += num;
   return suma;
+  //return nums.reduce((a,b) => a + b, 0);  Mismo resultado con el método reduce, con el que solo tendriamos una linea de código.
 }
 
 const numbers = [1, 2, 3];
 console.log(sum(numbers[0], numbers[1], numbers[2]));
+
+
+
